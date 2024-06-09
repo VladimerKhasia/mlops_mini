84d5f05f6fa8e37c2a4d158a144980bc0dffe197
Vladimer Khasia <vladimer.khasia.1@gmail.com>
2024-06-09 13:11:35 +0400

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,17 @@
-# def test_root(client):
-#     re = client.get("/")
-#     assert re.status_code == 200
-#     assert re.json() == {"mlops_mini": "This is a small end-to-end AI project for you!"}
-    
-def test_root(mocker, client):
-    mocker.patch('src.app.agent_service.AutoModelForCausalLM.from_pretrained', return_value=None)
-    mocker.patch('src.app.agent_service.conversational_agent', return_value=None)
+def test_root(client):
     re = client.get("/")
     assert re.status_code == 200
-    assert re.json() == {"mlops_mini": "This is a small end-to-end AI project for you!"}    
+    assert re.json() == {"mlops_mini": "This is a small end-to-end AI project for you!"}
+    
+# def test_mocked_root(mocker, client):
+#     mocker.patch('src.app.agent_service.AutoModelForCausalLM.from_pretrained', return_value=None)
+#     mocker.patch('src.app.agent_service.conversational_agent', return_value=None)
+#     re = client.get("/")
+#     assert re.status_code == 200
+#     assert re.json() == {"mlops_mini": "This is a small end-to-end AI project for you!"}    
+
+## use test_mocked_root if you add pytest-mock in .github/workflows/main.yaml file:
+      # - name: run pytest tests
+      #   run: |
+      #     pip install pytest pytest-mock
+      #     pytest -v -s -x
